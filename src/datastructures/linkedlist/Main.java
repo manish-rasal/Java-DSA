02b89462276f1c377a558a0027e13a111567f364
manrasal <manishrasal2000@gmail.com>
2024-04-11 15:20:06 +0530

    Linked Lists

---
+++ src/datastructures/linkedlist/Main.java
@@ -0,0 +1,11 @@
+package datastructures.linkedlist;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList(4);
+        linkedList.printList();
+        linkedList.getHead();
+        linkedList.getTail();
+        linkedList.getLength();
+    }
+}
