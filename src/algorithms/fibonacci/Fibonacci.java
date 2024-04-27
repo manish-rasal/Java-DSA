4d1d57e30ea01b0cd398d16a8b851650d1e32af5
manrasal <manishrasal2000@gmail.com>
2024-04-27 18:11:28 +0530

    Bottom Up

--- src/algorithms/fibonacci/Fibonacci.java
+++ src/algorithms/fibonacci/Fibonacci.java
@@ -15,8 +15,23 @@ public class Fibonacci {
         memo[n] = fib(n - 1) + fib(n - 2);
         return memo[n];
     }
+
+    /**
+     * The below method uses bottom-up (iterative approach)
+     */
+
+    public static int fibonacci(int n) {
+        int[] fibList = new int[n + 1];
+        fibList[1] = 1;
+        for(int i = 2; i <= n; i++) {
+            fibList[i] = fibList[i - 1] + fibList[i - 2];
+        }
+        return fibList[n];
+    }
+
     public static void main(String[] args) {
         System.out.println(fib(20));
+        System.out.println(fibonacci(20));
         System.out.println(counter);
     }
 }
